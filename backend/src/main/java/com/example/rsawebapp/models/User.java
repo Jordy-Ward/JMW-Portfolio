1f4901cc83f78ed2bce78130d111c4b509efbc46
Jordy Michael Ward <jordyward041@gmail.com>
2025-08-14 14:48:22 +0200

    Began creating the user model for an entity in the database. Also researched stack profile for data base

---
+++ backend/src/main/java/com/example/rsawebapp/models/User.java
@@ -0,0 +1,18 @@
+package com.example.rsawebapp.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String publicKey;
+}
