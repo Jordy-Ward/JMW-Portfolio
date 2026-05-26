579245479dc1a5493ee51caca829846ea0369e4a
Jordan Michael Ward <jordanward041@gmail.com>
2026-05-26 16:01:20 +0200

    remove(Edit photos)

--- frontend/src/config/photos.js
+++ frontend/src/config/photos.js
@@ -1,22 +1,17 @@
 // Photo gallery configuration
 export const photoGallery = [
   { id: 1, src: "/family.jpeg" },
-  { id: 2, src: "/guitar.jpeg" },
-  { id: 3, src: "/hackathon.jpeg" },
-  { id: 4, src: "/paintball.jpeg" },
-  { id: 5, src: "/georgia.jpeg" },
-  { id: 6, src: "/tank.jpeg" },
-  { id: 7, src: "/luke.jpeg" },
-  { id: 8, src: "/daddas.jpeg" },
-  { id: 9, src: "/flyingcat.jpeg" },
-  { id: 10, src: "/sleepytank.jpeg" },
-  { id: 11, src: "/GeorgiaVicGrad.jpg" },
-  { id: 12, src: "/gradFamBib.jpg" },
-  { id: 13, src: "/GrayMichJoshGrad.jpg" },
-  { id: 14, src: "/joGrad.jpg" },
-  { id: 15, src: "/rhysSunset.jpg" },
-  { id: 16, src: "/overheadPress.jpg" },
-  { id: 17, src: "/gymPose.jpg" }
+  { id: 2, src: "/hackathon.jpeg" },
+  { id: 3, src: "/georgia.jpeg" },
+  { id: 4, src: "/daddas.jpeg" },
+  { id: 5, src: "/flyingcat.jpeg" },
+  { id: 6, src: "/GeorgiaVicGrad.jpg" },
+  { id: 7, src: "/gradFamBib.jpg" },
+  { id: 8, src: "/GrayMichJoshGrad.jpg" },
+  { id: 9, src: "/joGrad.jpg" },
+  { id: 10, src: "/rhysSunset.jpg" },
+  { id: 11, src: "/overheadPress.jpg" },
+  { id: 12, src: "/gymPose.jpg" }
 ];
 
 // Helper function to shuffle array (Fisher-Yates algorithm)
