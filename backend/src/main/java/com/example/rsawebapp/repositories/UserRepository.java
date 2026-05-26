81526aa664441de7cd9abf4df68b5a5208e5d330
Jordan Michael Ward <jordanward041@gmail.com>
2026-05-26 15:54:08 +0200

    remove: Delete Spring Boot backend

--- backend/src/main/java/com/example/rsawebapp/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.rsawebapp.repositories;
-
-import com.example.rsawebapp.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
